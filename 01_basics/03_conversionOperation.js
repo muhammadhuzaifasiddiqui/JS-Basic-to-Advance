1b7bbc91210b1c800ad89994e988f55bc86d97a5
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-07-21 05:29:25 +0000

    Add conversion operation examples in JavaScript

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33aaa";
+
+console.log(typeof score); // number
+console.log(typeof (score)); // number
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); // 33
+
+// "33" => 33
+// "33aaa" => Nan
+// true => 1; false => 0
+
+let isLoggedin = ""
+let booleanIsLoggedIn = Boolean(isLoggedin);
+console.log(booleanIsLoggedIn); // true
+
+// 1 => true; 0 => false
+/// "" => false
+// "hitesh" =  true
+
+let someNumber = 35;
+let stringNumber = String(someNumber);
+console.log(stringNumber); // "35"
+console.log(typeof stringNumber); // string
