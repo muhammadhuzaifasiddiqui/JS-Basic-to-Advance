be5aa203595c771bf1e3a9c956332541628e4363
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-07-21 09:50:29 +0000

    Comment out unused variable declarations and functions in datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,8 +1,8 @@
 // const score = 100
-const scoreValue = 100.3
+// const scoreValue = 100.3
 
 // const isLoggedIn  = false
-const outsideTemp = null
+// const outsideTemp = null
 // let userEmail;
 
 // const id = Symbol("123");
@@ -12,14 +12,39 @@ const outsideTemp = null
 
 // const bigNumber = 1234567890123456789012345678901234567890n; // BigInt
 
-const heros = ["Thor", "Ironman", "Hulk"];
-let myObj = {
-    name: "Huzaifa",
-    age: 27,
-}
+// const heros = ["Thor", "Ironman", "Hulk"];
+// let myObj = {
+//     name: "Huzaifa",
+//     age: 27,
+// }
 
-const myFunction = function() {
-    console.log("Hellow World!");
-}
+// const myFunction = function() {
+//     console.log("Hellow World!");
+// }
 
-console.log(typeof scoreValue);
+// console.log(typeof scoreValue);
+
+// let myfacebookname = "Huzaifa"
+
+// let anothername = myfacebookname
+// console.log(anothername);
+
+console.log("myYouTubeName");
+console.log("anotherName");
+
+let userOne = {
+  email: "userone@gmail.com",
+  upi: "userone@upi"
+};
+
+let userTwo = {
+  email: "usertwo@gmail.com",
+  upi: "usertwo@upi"
+};
+
+userTwo = userOne;
+
+userTwo.email = "huzaifa@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
