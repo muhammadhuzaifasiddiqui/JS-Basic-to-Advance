9809d3f8e71690de009ec8510cd8527d5881a65f
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-07-21 04:48:14 +0000

    Add variable declarations and data type examples in separate JavaScript files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountEmail = "huzaifa@google.com"
+var accountPass = "12a12b12"
+accountCity = "Jaipur"
+let accountState;
+//accountId = 2
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; ///treat all js code as newer version
+
+// alert(3+3) we are using node js, not browser
+
+console.log(3+5);  console.log("person1"); // readability should be high
+
+let name = "Huzaifa"
+let age = 27
+let loggedIn = false
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //null
