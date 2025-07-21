46d169993807ce9843d883242e83af896fa28efe
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-07-21 05:37:31 +0000

    Comment out conversion operation examples in JavaScript

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,25 +1,30 @@
-let score = "33aaa";
+//let score = "33aaa";
 
-console.log(typeof score); // number
-console.log(typeof (score)); // number
+//console.log(typeof score); // number
+//console.log(typeof (score)); // number
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber); // number
-console.log(valueInNumber); // 33
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); // number
+//console.log(valueInNumber); // 33
 
 // "33" => 33
 // "33aaa" => Nan
 // true => 1; false => 0
 
-let isLoggedin = ""
-let booleanIsLoggedIn = Boolean(isLoggedin);
-console.log(booleanIsLoggedIn); // true
+//let isLoggedin = ""
+//let booleanIsLoggedIn = Boolean(isLoggedin);
+//console.log(booleanIsLoggedIn); // true
 
 // 1 => true; 0 => false
 /// "" => false
 // "hitesh" =  true
 
-let someNumber = 35;
-let stringNumber = String(someNumber);
-console.log(stringNumber); // "35"
-console.log(typeof stringNumber); // string
+//let someNumber = 35;
+//let stringNumber = String(someNumber);
+//console.log(stringNumber); // "35"
+//console.log(typeof stringNumber); // string
+
+let value = 3;
+let negVal = -value;
+console.log(negVal); // -3
+console.log(typeof negVal); // number   
