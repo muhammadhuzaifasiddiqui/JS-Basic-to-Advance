7c9bdfbbbf1e80027017f63f467cbb475853696b
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-07-22 06:00:59 +0000

    Add string and number examples in separate JavaScript files

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+const gameName = new String("Metal Slug");
+const newString = gameName.substring(0, 5)  + " Super " + gameName.substring(6, 10)  + " Game";
+console.log(newString);
+
+// const nme = "HUZAIFA";
+// const number = 50;
+
+// // // console.log(nme + number + " is my name and number");
+// // console.log(`My name is ${nme.toLocaleLowerCase()} and my repo count is ${number}`);
+
+// console.log(gameName[0]); // String object
+// console.log(gameName.__proto__); // true
+// console.log(gameName.constructor); // String constructor
+// console.log(gameName.length); // 11
+// console.log(gameName.toUpperCase()); // 'M'
+// console.log(gameName.toLowerCase()); // 'M'
+// console.log(gameName.charAt(9)); // 'M'
+// console.log(gameName.indexOf("l")); // 6
+// console.log(gameName);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.length);
+// console.log(gameName.charAt(0));
+// console.log(gameName.indexOf("Slug"));
+// console.log(gameName.lastIndexOf("Slug"));
+// console.log(gameName.slice(0, 4));
+// console.log(gameName.substring(0, 4));
+// console.log(gameName.replace("Metal", "Super"));
+// console.log(gameName.replaceAll("Slug", "Super"));
+// console.log(gameName.split("-"));
+// console.log(gameName.startsWith("Metal"));
+// console.log(gameName.endsWith("Slug"));
+// console.log(gameName.includes("Slug"));
+// console.log(gameName.trim()); // Removes whitespace from both ends
+// console.log(gameName.trimStart()); // Removes whitespace from the start
+// console.log(gameName.trimEnd()); // Removes whitespace from the end
