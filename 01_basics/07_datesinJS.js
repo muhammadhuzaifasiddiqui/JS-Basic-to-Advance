e20d69d533b4686700d70e6cbb79c49115e1e29a
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-07-22 09:53:18 +0000

    Remove commented-out date examples and clean up unused code in datesinJs.js

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,41 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2025, 0, 2);
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toString());
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date(2025, 0, 2, 6, 50);
+// console.log(myCreatedDate.toLocaleString());
+
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date("01-02-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date()
+console.log(newDate);
+
+
+
+
+
+
+
+
+
+
+
+
+
