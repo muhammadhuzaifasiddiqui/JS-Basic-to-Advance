da6c0e8fa1d64efbf1288881499ad1c5408772f1
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-07-23 06:03:42 +0000

    Add initial array examples and console logs in 01_arrays.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,69 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ['Fateh-Sultan', 'Ertugrul', 'Malik Shah', 'Alp Arslan']
+const myEnemies = new Array('Israel', 'America', 'Britain', 'India')
+
+console.log("A ", myArr);
+console.log(myArr.slice(1,4));
+console.log("B ", myArr);
+console.log(myArr.splice(1,4));
+console.log("C ", myArr);
+
+
+
+
+
+
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+// console.log(myArr);
+
+// console.log(myArr.includes(4));
+// console.log(myHeros.includes('Ertugrul'));
+// console.log(myEnemies.includes('Israel'));
+
+// console.log(myArr.indexOf(5));
+// console.log(myHeros.indexOf('Ertugrul'));
+// console.log(myEnemies.indexOf('Israel'));
+
+// console.log(myArr.indexOf(8));
+// console.log(myHeros.indexOf('Tipu-Sultan'));
+// console.log(myEnemies.indexOf('France'));
+
+// myArr.push(6);
+// myHeros.push('Sultan Saladin');
+// myEnemies.push('Russia');
+// console.log(myArr);
+// console.log(myHeros);
+// console.log(myEnemies);
+// myArr.pop();
+// myHeros.pop();
+// myEnemies.pop();
+// console.log(myArr);
+// console.log(myHeros);
+// console.log(myEnemies);
+// myArr.unshift(-1);
+// myHeros.unshift('Khalid-bin-Waleed');
+// myEnemies.unshift('Abu-Lahab');
+// console.log(myArr);
+// console.log(myHeros);
+// console.log(myEnemies);
+// myArr.shift();
+// myHeros.shift();
+// myEnemies.shift();
+// console.log(myArr);
+// console.log(myHeros);
+// console.log(myEnemies);
+
+// console.log(myArr);
+// console.log(myHeros);
+// console.log(myEnemies);
+// console.log(myHeros[0]);
+// console.log(myHeros[1]);
+// console.log(myHeros[2]);
+// console.log(myHeros[3]);
+// console.log(myEnemies[0]);
+// console.log(myEnemies[1]);
+// console.log(myEnemies[2]);
+// console.log(myEnemies[3]);
+// console.log(myArr[3]);
